759547ad6e00e2017234c74e165d98e2b3d00648
immortalWebDev <pgbadgujar007@gmail.com>
2024-08-27 19:33:12 +0530

    Added js logic for qr code

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const input = document.getElementById("inp");
+const qrcodeContainer = document.getElementById("qrcode-container");
+const qrimg = document.getElementById("qrimg");
+const btn = document.getElementById("btn");
+
+const generateQRCode = (data) => {
+  return `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${encodeURIComponent(
+    data
+  )}`;
+};
+
+const showPlaceholderQRCode = () => {
+  qrimg.setAttribute("src", generateQRCode("https://www.google.com"));
+  qrcodeContainer.style.display = "flex";
+};
+
+btn.addEventListener("click", () => {
+  if (input.value) {
+    qrimg.setAttribute("src", generateQRCode(input.value));
+
+    qrcodeContainer.style.display = "flex";
+  } else {
+    showPlaceholderQRCode();
+  }
+});
+
+showPlaceholderQRCode();
